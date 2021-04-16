7453a87c9ab17ccfe5565ef16be0fcd1496a5473
Bjorn Yourey <byourey7783@gmail.com>
2021-04-16 11:54:29 -0400

    added routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const Router = require("express").Router();
+const path = require("path");
+
+Router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+
+Router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public.exercise.html"));
+})
+
+Router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public.stats.html"));
+})
+
--- server.js
+++ server.js
@@ -5,3 +5,31 @@ require('dotenv').config();
 
 // Routes
 const htmlRoutes = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+const { urlencoded } = require("body-parser");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+
+});
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+

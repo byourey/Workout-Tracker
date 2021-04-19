ab77d8659121e9935fd2e3e7c460479a6064393d
Bjorn Yourey <byourey7783@gmail.com>
2021-04-19 12:23:19 -0400

    -

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
+const db = require("../models");
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
